62e06bec753894a4d0ade2284a71aa5c26c43e3a
Rahul <rahulnair87@gmail.com>
2024-03-14 13:47:53 +0530

    simplified the steps

--- check.js
+++ check.js
@@ -1,15 +1,44 @@
-const {Block, hash_transaction} = require ("@emurgo/cardano-serialization-lib-nodejs")
-const fs = require ("fs")
-const path = require ("path")
+const {
+  Block,
+  hash_transaction,
+} = require("@emurgo/cardano-serialization-lib-nodejs");
+const fs = require("fs");
+const path = require("path");
 
-const cborHex = fs.readFileSync(path.join(__dirname,'cbor.txt')).toString()
-const block = Block.from_wrapped_bytes(Buffer.from(cborHex,'hex'))
+// Replace with Transaction Hash for which CBOR is needed
+const txnHash =
+  "00a5d69211a2c4c1ea2013206b8fc41c300cac76f84c4c026647d8a4d8afad92";
+
+const strData = fs.readFileSync(path.join(__dirname, "output.txt")).toString();
+
+let cborHexFound = false;
+let extractedData = "";
+
+const cborIndex = strData.indexOf("cbor_hex");
+if (cborIndex !== -1) {
+  const colonIndex = strData.indexOf(":", cborIndex);
+  const openingQuoteIndex = strData.indexOf('"', colonIndex);
+
+  if (openingQuoteIndex !== -1) {
+    const closingQuoteIndex = strData.indexOf('"', openingQuoteIndex + 1);
+    if (closingQuoteIndex !== -1) {
+      extractedData = strData.substring(
+        openingQuoteIndex + 1,
+        closingQuoteIndex
+      );
+      cborHexFound = true;
+    }
+  }
+}
+
+const cborHex = extractedData;
+const block = Block.from_wrapped_bytes(Buffer.from(cborHex, "hex"));
 
 for (let i = 0; i < block.transaction_bodies().len(); i++) {
-    const tx = block.transaction_bodies().get(i);
-    const hash = hash_transaction(tx).to_hex();
-    if (hash === '00a5d69211a2c4c1ea2013206b8fc41c300cac76f84c4c026647d8a4d8afad92') {
-   //   console.log(tx.to_hex());
-        console.log(tx.to_json())
-    }
+  const tx = block.transaction_bodies().get(i);
+  const hash = hash_transaction(tx).to_hex();
+  if (hash === txnHash) {
+    console.log(tx.to_hex()); //CBOR hex
+    console.log(tx.to_json()); //CBOR JSON
+  }
 }
---
+++ execOura.js
@@ -0,0 +1,53 @@
+const { spawn } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+// Sample Oura command to execute
+// 'oura daemon --config daemon.toml --cursor 116956876,2760ee4207c51344154116cb465138a99eb7214cd8c2fdf1486cd81994ce50e0'
+
+//replace Param1
+const parm1 = "116956876";
+
+//replace param2
+const parm2 =
+  "2760ee4207c51344154116cb465138a99eb7214cd8c2fdf1486cd81994ce50e0";
+
+// Use the full path to the 'oura' executable
+const ouraPath = "/usr/local/bin/oura";
+const args = [
+  "daemon",
+  "--config",
+  "daemon.toml",
+  "--cursor",
+  `${parm1},${parm2}`,
+];
+
+// Execute the command
+const child = spawn(ouraPath, args);
+
+// Store the output in a file
+const outputFile = "output.txt";
+const outputStream = fs.createWriteStream(outputFile);
+
+// Pipe the child process stdout to the output file
+child.stdout.pipe(outputStream);
+// Set a timeout to kill the child process after 3 seconds
+const timeout = setTimeout(() => {
+  console.log("Timeout reached. Stopping execution.");
+  child.kill("SIGINT"); // Send a SIGINT signal to gracefully terminate the child process
+}, 3000); // 3 seconds
+
+// Listen for the child process to exit
+child.on("exit", (code, signal) => {
+  clearTimeout(timeout); // Clear the timeout when the process exits
+  console.log(`Child process exited with code ${code} and signal ${signal}`);
+});
+
+// Listen for any output from the child process
+child.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+child.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+});
